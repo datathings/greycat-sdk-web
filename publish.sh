437a5caaa8d926b485eae348c2f2b010a9f8ba9a
Maxime Tricoire <maxime.tricoire@datathings.com>
2023-08-21 14:46:37 +0200

    CI publish prepapre

---
+++ publish.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+VERSION_MAJOR_MINOR=`cat VERSION`
+VERSION=${VERSION:-"0.0.0"}
+
+echo "${VERSION_MAJOR_MINOR} / ${VERSION}"
+
+sha256_hash=$(echo -n "$GET_GC_CI_PASS" | openssl dgst -sha256 | cut -d ' ' -f2)
+base64url_token=$(echo -n "ci:$sha256_hash" | base64 -w 0 )
+token=$(curl -s -d "[\"${base64url_token}\", false]" -X POST https://get.greycat.io/runtime::User::login | tr -d '"')
+
+cd dist
+
+file="dist.zip"
+zip -r $file ui
+
+curl -s -X PUT -H "Authorization: $token" -T $file "https://get.greycat.io/files/sdk/js/${CI_COMMIT_REF_NAME}/${VERSION_MAJOR_MINOR}/${VERSION}.zip"
+curl -s -X PUT -H "Authorization: $token" -T $file "https://get.greycat.io/files/sdk/js/${CI_COMMIT_REF_NAME}/latest.zip"
+curl -s -X PUT -H "Authorization: $token" -T ui/package.tgz "https://get.greycat.io/files/sdk/js/${CI_COMMIT_REF_NAME}/${VERSION_MAJOR_MINOR}/${VERSION}-package.tgz"
