abf0ca68acc3115acc6a7181588de4918ff05f02
Maxime Tricoire <maxime.tricoire@datathings.com>
2023-08-21 14:30:59 +0200

    migrate v5 chart complete example to v6

--- pages/v6/task-manager/index.ts
+++ pages/v6/task-manager/index.ts
@@ -5,6 +5,5 @@ import '../../../src';
 
 const greycat = await GreyCat.init({ url: new URL('http://localhost:8080') });
 
-
 const task = await greycat.call('project::whatever');
 console.log(task);
