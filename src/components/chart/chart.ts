a019ba3fbe8553b298a0f755ae6eedb61caa1c45
Maxime Tricoire <maxime.tricoire@datathings.com>
2023-09-07 10:43:30 +0200

    gui-chart: alt+wheel for y axes panning

--- src/components/chart/chart.ts
+++ src/components/chart/chart.ts
@@ -204,7 +204,27 @@ export class GuiChart extends HTMLElement {
     this.addEventListener('wheel', throttle((event: WheelEvent) => {
       // if this is too slow, maybe cache xRange, yRange
       const { xRange, yRange, xScale: scale, yScales } = this._computed;
-      if (
+      if (event.shiftKey) {
+        // x axis panning
+        const [min, max] = scale.range();
+        const dx = (Math.abs(max - min) / (this._config.xAxis.ratio ?? 100)) * (event.deltaY > 0 ? -1 : 1);
+        const from = this._config.xAxis.min = scale.invert(min + dx);
+        const to = this._config.xAxis.max = scale.invert(max + dx);
+        this.dispatchEvent(new GuiChartSelectionEvent(from, to));
+        this._compute();
+        this.update();
+      } else if (event.altKey) {
+        // y axes panning
+        for (const [name, scale] of Object.entries(yScales)) {
+          const axis = this._config.yAxes[name];
+          const [min, max] = scale.range();
+          const d = (Math.abs(max - min) / (axis.ratio ?? 100)) * (event.deltaY > 0 ? -1 : 1);
+          axis.min = scale.invert(min + d);
+          axis.max = scale.invert(max + d);
+        }
+        this._compute();
+        this.update();
+      } else if (
         this._cursor.x < xRange[0] &&
         this._cursor.y <= yRange[0] &&
         this._cursor.y >= yRange[1]
@@ -246,16 +266,8 @@ export class GuiChart extends HTMLElement {
         // x axis zoom
         const [min, max] = scale.range();
         const d = (Math.abs(max - min) / (this._config.xAxis.ratio ?? 100)) * (event.deltaY > 0 ? 1 : -1);
-        const from = this._config.xAxis.min = Math.floor(+scale.invert(min - d));
-        const to = this._config.xAxis.max = Math.ceil(+scale.invert(max + d));
-        this.dispatchEvent(new GuiChartSelectionEvent(from, to));
-        this._compute();
-        this.update();
-      } else if (event.shiftKey) {
-        const [min, max] = scale.range();
-        const dx = (Math.abs(max - min) / (this._config.xAxis.ratio ?? 100)) * (event.deltaY > 0 ? -1 : 1);
-        const from = this._config.xAxis.min = Math.floor(+scale.invert(min + dx));
-        const to = this._config.xAxis.max = Math.ceil(+scale.invert(max + dx));
+        const from = this._config.xAxis.min = scale.invert(min - d);
+        const to = this._config.xAxis.max = scale.invert(max + d);
         this.dispatchEvent(new GuiChartSelectionEvent(from, to));
         this._compute();
         this.update();
