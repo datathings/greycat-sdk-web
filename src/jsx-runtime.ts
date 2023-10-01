4a6322915ab878e6534c0d5af22852d6fe8f7a26
Maxime Tricoire <maxime.tricoire@datathings.com>
2023-10-01 22:51:31 +0200

    explain eslint disable

--- src/jsx-runtime.ts
+++ src/jsx-runtime.ts
@@ -50,6 +50,8 @@ export function createElement<K extends keyof HTMLElementTagNameMap>(
         if (key.startsWith('on')) {
           element.addEventListener(key.substring(2), value);
         } else if (key in element) {
+          // safety: we just validated that 'key' was a property in 'element'
+          // therefore we can, at least, set it
           // eslint-disable-next-line @typescript-eslint/no-explicit-any
           (element as any)[key] = value;
         } else {
