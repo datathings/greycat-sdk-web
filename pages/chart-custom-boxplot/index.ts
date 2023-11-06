55133553ceb7c7e488d243f757221265c390dce4
Max <max.cruz@datathings.com>
2023-11-06 16:17:44 +0100

    vertical & horizontal boxplots

--- pages/chart-custom-boxplot/index.ts
+++ pages/chart-custom-boxplot/index.ts
@@ -13,8 +13,12 @@ const boxplot = await greycat.default.call<util.BoxPlotFloat>('project::boxplot_
 
 const chart1 = document.createElement('gui-chart');
 chart1.style.height = '80vh';
-chart1.style.width = '75%';
+chart1.style.width = '100%';
 chart1.style.margin = 'auto';
+const chart2 = document.createElement('gui-chart');
+chart2.style.height = '80vh';
+chart2.style.width = '100%';
+chart2.style.margin = 'auto';
 
 chart1.config = {
   table: { cols: [[]] },
@@ -31,7 +35,7 @@ chart1.config = {
           min: yScale(boxplot.min),
           q1: yScale(boxplot.percentile25),
           q3: yScale(boxplot.percentile75),
-          x: xScale(5),
+          crossValue: xScale(5),
         };
 
         const boxPlotOptions: BoxPlotOptions = {
@@ -39,6 +43,7 @@ chart1.config = {
           iqrColor: s.color,
           whiskerColor: s.color,
           medianColor: s.color,
+          orientation: 'vertical',
         };
 
         ctx.boxPlot(boxPlotCanvas, boxPlotOptions);
@@ -59,8 +64,57 @@ chart1.config = {
       max: boxplot.max,
     },
   },
+  selection: false,
+  cursor: false,
+} satisfies ChartConfig;
+chart2.config = {
+  table: { cols: [[]] },
+  series: [
+    {
+      type: 'custom',
+      yAxis: 'left',
+      yCol: 0,
+      hideInTooltip: true,
+      draw(ctx, s, xScale, yScale) {
+        const boxPlotCanvas: BoxPlotCanvas = {
+          max: xScale(boxplot.max),
+          median: xScale(boxplot.percentile50),
+          min: xScale(boxplot.min),
+          q1: xScale(boxplot.percentile25),
+          q3: xScale(boxplot.percentile75),
+          crossValue: yScale(5),
+        };
+
+        const boxPlotOptions: BoxPlotOptions = {
+          width: 200,
+          iqrColor: s.color,
+          whiskerColor: s.color,
+          medianColor: s.color,
+          orientation: 'horizontal',
+        };
+
+        ctx.boxPlot(boxPlotCanvas, boxPlotOptions);
+      },
+    },
+  ],
+  xAxis: {
+    scale: 'linear',
+    min: boxplot.min,
+    max: boxplot.max,
+  },
+  yAxes: {
+    left: {
+      min: 0,
+      max: 10,
+      hook(axis) {
+        axis.ticks(0);
+      },
+    },
+  },
+  selection: false,
   cursor: false,
 } satisfies ChartConfig;
 
 app.main.style.overflow = 'auto';
-app.main.replaceChildren(chart1);
+app.main.classList.add('grid');
+app.main.replaceChildren(chart1, chart2);
