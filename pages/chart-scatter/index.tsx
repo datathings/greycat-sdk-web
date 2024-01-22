0d8e65b4f078d7113c36d4da5df862416dfaf0d5
Max <max.cruz@datathings.com>
2024-01-22 14:15:02 +0100

    handle unordered  x y  numerical values

--- pages/chart-scatter/index.tsx
+++ pages/chart-scatter/index.tsx
@@ -54,13 +54,15 @@ chart.setConfig({
   table: {
     cols: [
       Array.from({ length: 1000 }, d3.randomNormal(5, 1)),
+      Array.from({ length: 1000 }, d3.randomNormal(5, 1)),
     ],
   },
   series: [
     {
       type: 'scatter',
       yAxis: 'left',
-      yCol: 0,
+      yCol: 1,
+      xCol: 0,
       width: 3,
       markerWidth: 5,
     },
@@ -68,7 +70,7 @@ chart.setConfig({
   xAxis: {
     scale: 'linear',
     min: 0,
-    max: 1000,
+    max: 10,
   },
   yAxes: { left: { scale: 'linear', min: 0, max: 10 } },
   cursor: true,
