40beb131498b852a71b7eeac67b08edb5b598191
Max <max.cruz@datathings.com>
2024-02-09 12:02:36 +0100

    refactored heatmap

--- pages/heatmap/index.ts
+++ pages/heatmap/index.ts
@@ -1,6 +1,7 @@
 import type { core } from '@greycat/sdk';
 import '../layout';
 import './index.css';
+import { HeatmapConfig } from '../../src';
 
 const app = document.createElement('app-layout');
 app.title = 'Heatmap';
@@ -11,11 +12,22 @@ document.body.prepend(app);
 const table = await greycat.default.call<core.Table>('project::heatmap');
 
 const heatmap = document.createElement('gui-heatmap');
-heatmap.style.width = '1024px';
+heatmap.style.width = '100%';
 heatmap.style.height = '768px';
 app.main.appendChild(heatmap);
 
-heatmap.xLabels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-heatmap.yLabels = ["Paris", "London", "New-York", "Beijing", "Perth", "Oslo"];
-heatmap.value = table;
-heatmap.colorScaleWidth = 50;
+const config: HeatmapConfig = {
+  table: table,
+  xAxis: {
+    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+  },
+  yAxis: {
+    labels: ['Paris', 'London', 'New-York', 'Beijing', 'Perth', 'Oslo'],
+  },
+  colorScaleColors: ['blue', '#ff0000'],
+  tooltip: {
+    position: 'follow',
+  },
+};
+
+heatmap.config = config;
