52801f20e5e3c8ff7ac8617c9385021ac11af002
Maxime Tricoire <maxime.tricoire@datathings.com>
2024-04-25 09:41:48 +0200

    fix merge

--- playground/pages/chart/index.tsx
+++ playground/pages/chart/index.tsx
@@ -1,106 +1,5 @@
-<<<<<<< HEAD:playground/pages/chart/index.tsx
-import { GreyCat, type GuiChart, IndexedDbCache, type core } from '@greycat/web';
+import { GreyCat, type GuiChart, type SerieStyle, IndexedDbCache, type core } from '@greycat/web';
 import '@/common';
-=======
-import { core } from '@greycat/sdk';
-import '../layout';
-import { SerieStyle } from '../../src';
-
-const app = document.createElement('app-layout');
-app.title = 'Chart';
-await app.init();
-
-document.body.prepend(app);
-
-const nbRows = (
-  <input
-    name="nb-rows"
-    id="nb-rows"
-    type="number"
-    min="1"
-    max="10000"
-    value="150"
-    oninput={randomize}
-  />
-) as HTMLInputElement;
-app.actions.prepend(
-  <>
-    <li>{nbRows}</li>
-    <li>
-      <a href="#" onclick={randomize}>
-        Randomize
-      </a>
-    </li>
-    <li>
-      <a
-        href="#"
-        onclick={() => {
-          if (chart.config.tooltip === undefined) {
-            chart.config.tooltip = {};
-          }
-          chart.config.tooltip.position = 'top-left';
-        }}
-      >
-        Top-left
-      </a>
-    </li>
-    <li>
-      <a
-        href="#"
-        onclick={() => {
-          if (chart.config.tooltip === undefined) {
-            chart.config.tooltip = {};
-          }
-          chart.config.tooltip.position = 'top-right';
-        }}
-      >
-        Top-right
-      </a>
-    </li>
-    <li>
-      <a
-        href="#"
-        onclick={() => {
-          if (chart.config.tooltip === undefined) {
-            chart.config.tooltip = {};
-          }
-          chart.config.tooltip.position = 'bottom-left';
-        }}
-      >
-        Bottom-left
-      </a>
-    </li>
-    <li>
-      <a
-        href="#"
-        onclick={() => {
-          if (chart.config.tooltip === undefined) {
-            chart.config.tooltip = {};
-          }
-          chart.config.tooltip.position = 'bottom-right';
-        }}
-      >
-        Bottom-right
-      </a>
-    </li>
-    <li>
-      <a
-        href="#"
-        onclick={() => {
-          chart.config.cursor = !chart.config.cursor;
-        }}
-      >
-        Toggle cursor
-      </a>
-    </li>
-  </>,
-);
-
-const table = await greycat.default.call<core.Table>('project::chart', [150]);
-
-const chart = document.createElement('gui-chart');
-app.main.replaceChildren(chart);
->>>>>>> testing:pages/chart/index.tsx
 
 const LINE_COL = 0;
 const SCATTER_COL = 1;
@@ -121,7 +20,6 @@ const styleMapping: Record<string, SerieStyle> = {
   },
 };
 
-<<<<<<< HEAD:playground/pages/chart/index.tsx
 greycat.default = await GreyCat.init({
   cache: new IndexedDbCache('sdk-web-playground'),
 });
@@ -156,9 +54,8 @@ const chart = (
           yAxis: 'left',
           yCol: LINE_COL,
           width: 4,
-          lineTypeCol: LINE_TYPE_COL,
-          colorCol: LINE_COLOR_COL,
-          colorMapping: (v) => customColorMap[v.key as keyof typeof customColorMap],
+          styleCol: STYLE_COL,
+          styleMapping: (v) => styleMapping[v.key],
         },
         {
           type: 'line+scatter',
@@ -270,66 +167,6 @@ document.body.appendChild(
     {chart}
   </app-layout>,
 );
-=======
-chart.config = {
-  cursor: true,
-  selection: {
-    orientation: 'both',
-  },
-  xAxis: {
-    cursorFormat: '.0f',
-    // ratio: 0,
-  },
-  yAxes: {
-    left: {
-      min: 0,
-      max: 200,
-      format: '.2s',
-    },
-    right: {
-      position: 'right',
-    },
-  },
-  table,
-  series: [
-    {
-      title: 'Custom',
-      type: 'line',
-      curve: 'step-after',
-      yAxis: 'left',
-      yCol: LINE_COL,
-      width: 4,
-      styleCol: STYLE_COL,
-      styleMapping: (v) => styleMapping[v.key],
-    },
-    {
-      type: 'line+scatter',
-      yAxis: 'left',
-      plotRadius: 3,
-      yCol: SCATTER_COL,
-    },
-    {
-      type: 'line+area',
-      yAxis: 'right',
-      yCol: AREA_COL,
-    },
-    {
-      type: 'bar',
-      yAxis: 'left',
-      yCol: BAR_COL,
-      width: 10,
-      color: 'orange',
-    },
-    {
-      type: 'line+area',
-      yAxis: 'left',
-      yCol: AREA_COL,
-      yCol2: LINE_COL,
-      hideInTooltip: true,
-    },
-  ],
-};
->>>>>>> testing:pages/chart/index.tsx
 
 // eslint-disable-next-line no-inner-declarations
 async function randomize() {
