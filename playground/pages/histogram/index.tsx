f6b2c2a8335e3e19f2fdb01db45393351ddfac4a
Max <max.cruz@datathings.com>
2024-08-27 16:47:49 +0200

    clean

--- playground/pages/histogram/index.tsx
+++ playground/pages/histogram/index.tsx
@@ -5,7 +5,7 @@ await GreyCat.init({
   cache: new IndexedDbCache('sdk-web-playground'),
 });
 
-const a = (await $.default.call('project::histo_example', [])) as util.Histogram;
+const a = (await $.default.call('project::oned_histo_example', [])) as util.Histogram;
 document.body.appendChild(
   <app-layout title="Hello">
     <gui-histogram value={a}></gui-histogram>
