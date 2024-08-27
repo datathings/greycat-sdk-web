567253ec32748d795059b64231038465a47dfe60
Max <max.cruz@datathings.com>
2024-08-27 17:48:06 +0200

    log quantizer get bounds

--- src/components/histogram/histogram.ts
+++ src/components/histogram/histogram.ts
@@ -1,6 +1,8 @@
 import { ChartConfig, util } from '../../exports.js';
 
 export class GuiHistogram extends HTMLElement {
+  static GC_UTIL_THRESHOLD_LOG = 1e-4;
+
   private _value?: util.Histogram;
 
   constructor() {
@@ -48,6 +50,59 @@ export class GuiHistogram extends HTMLElement {
     if (quantizer instanceof util.LinearQuantizer) {
       const step = (quantizer.max - quantizer.min) / Number(quantizer.bins);
       return [quantizer.min + slot * step, quantizer.min + (slot + 1) * step];
+    } else if (quantizer instanceof util.LogQuantizer) {
+      const bins = Number(quantizer.bins);
+      let min = quantizer.min;
+      let max = quantizer.max;
+      if (quantizer.min > 0 && quantizer.max > 0) {
+        const logMin = Math.log(quantizer.min);
+        const logMax = Math.log(quantizer.max);
+        const step = (logMax - logMin) / bins;
+        if (slot !== 0) {
+          min = logMin + slot * step;
+        }
+        if (slot !== bins - 1) {
+          max = logMin + (slot + 1) * step;
+        }
+      } else {
+        if (min < 0 && max < 0) {
+          const logMin = Math.log(-max);
+          const logMax = Math.log(-min);
+          const step = (logMax - logMin) / bins;
+          slot = bins - 1 - slot;
+          min = -Math.exp(logMin + (slot + 1) * step);
+          max = -Math.exp(logMin + slot * step);
+        } else {
+          const thresholdLog = Math.log(GuiHistogram.GC_UTIL_THRESHOLD_LOG);
+          if (slot === bins / 2) {
+            min = -GuiHistogram.GC_UTIL_THRESHOLD_LOG;
+            max = GuiHistogram.GC_UTIL_THRESHOLD_LOG;
+          } else if (slot > bins / 2) {
+            const logMax = Math.log(max);
+            const step = logMax - thresholdLog;
+            if (slot == bins / 2 + 1) {
+              min = GuiHistogram.GC_UTIL_THRESHOLD_LOG;
+            } else {
+              min = Math.exp(thresholdLog + (slot - bins / 2 - 1) * step);
+            }
+            if (slot !== bins - 1) {
+              max = Math.exp(thresholdLog + (slot - bins / 2) * step);
+            }
+          } else {
+            const logMin = Math.log(-min);
+            const step = (thresholdLog - logMin) / (bins / 2);
+            if (slot !== 0) {
+              min = -Math.exp(logMin + slot * step);
+            }
+            if (slot === bins / 2 - 1) {
+              max = -GuiHistogram.GC_UTIL_THRESHOLD_LOG;
+            } else {
+              max = -Math.exp(logMin + (slot + 1) * step);
+            }
+          }
+        }
+      }
+      return [min, max];
     }
 
     return [0, 1];
