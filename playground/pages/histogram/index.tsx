3fbc49ddb0bed9fa4e1d58a50be86f3690893552
Max <max.cruz@datathings.com>
2024-08-28 17:20:43 +0200

    2d histogram

--- playground/pages/histogram/index.tsx
+++ playground/pages/histogram/index.tsx
@@ -5,9 +5,11 @@ await GreyCat.init({
   cache: new IndexedDbCache('sdk-web-playground'),
 });
 
-const a = (await $.default.call('project::oned_histo_example', [])) as util.Histogram;
+const a = (await $.default.call('project::one_d_histo_example', [])) as util.Histogram;
+const b = (await $.default.call('project::two_d_histo_example', [])) as util.Histogram;
 document.body.appendChild(
   <app-layout title="Hello">
+    <gui-histogram value={b}> </gui-histogram>
     <gui-histogram value={a}></gui-histogram>
   </app-layout>,
 );
--- src/components/histogram/histogram.ts
+++ src/components/histogram/histogram.ts
@@ -1,4 +1,4 @@
-import { ChartConfig, util } from '../../exports.js';
+import { ChartConfig, HeatmapConfig, util } from '../../exports.js';
 
 export class GuiHistogram extends HTMLElement {
   static GC_UTIL_THRESHOLD_LOG = 1e-4;
@@ -20,7 +20,31 @@ export class GuiHistogram extends HTMLElement {
 
   private render() {
     if (!this._value?.bins) return;
+    const quant = this._value.quantizer;
 
+    if (this._is_valid_quantizer(quant)) {
+      this._render_histogram(this._value.bins, quant);
+    } else if (quant instanceof util.MultiQuantizer) {
+      if (quant.dimensions.length === 1 && this._is_valid_quantizer(quant.dimensions[0])) {
+        this._render_histogram(this._value.bins, quant.dimensions[0]);
+      } else if (
+        quant.dimensions.length === 2 &&
+        this._is_valid_quantizer(quant.dimensions[0]) &&
+        this._is_valid_quantizer(quant.dimensions[1])
+      ) {
+        this._render_heatmap(this._value.bins, [quant.dimensions[0], quant.dimensions[1]]);
+      } else {
+        throw 'Not Supported';
+      }
+    } else {
+      throw 'Not Supported';
+    }
+  }
+
+  private _render_histogram(
+    bins: (number | bigint | null)[],
+    quantizer: util.LogQuantizer | util.LinearQuantizer,
+  ) {
     const config: ChartConfig = {
       xAxis: {},
       yAxes: { left: {} },
@@ -28,10 +52,10 @@ export class GuiHistogram extends HTMLElement {
     };
     const chart = document.createElement('gui-chart');
 
-    const data: number[][] = Array.from({ length: this._value.bins.length });
-    for (let index = 0; index < this._value.bins.length; index++) {
-      const bounds = this._get_bounds(index, this._value.quantizer);
-      data[index] = [bounds[0], bounds[1], Number(this._value.bins[index])];
+    const data: number[][] = Array.from({ length: bins.length });
+    for (let index = 0; index < bins.length; index++) {
+      const bounds = this._get_bounds(index, quantizer);
+      data[index] = [bounds[0], bounds[1], Number(bins[index])];
     }
 
     // Temp Fix to be removed once serie of type bar works with spanCol correctly
@@ -44,7 +68,47 @@ export class GuiHistogram extends HTMLElement {
     this.replaceChildren(chart);
   }
 
-  connectedCallback() {}
+  private _render_heatmap(
+    bins: (number | bigint | null)[],
+    quantizer: (util.LinearQuantizer | util.LogQuantizer)[],
+  ) {
+    const heatmap = document.createElement('gui-heatmap');
+
+    const data = Array.from({ length: Number(quantizer[0].bins) }, (_) =>
+      Array.from({ length: Number(quantizer[1].bins) }),
+    );
+
+    const xLabels = [];
+    const yLabels = [];
+
+    const cols = Number(quantizer[0].bins);
+    for (let col = 0; col < quantizer[0].bins; col++) {
+      const xbounds = this._get_bounds(col, quantizer[0]);
+      xLabels.push(xbounds[0].toString());
+
+      for (let row = 0; row < quantizer[1].bins; row++) {
+        if (col === 0) {
+          const ybounds = this._get_bounds(row, quantizer[1]);
+          yLabels.push(ybounds[0].toString());
+        }
+
+        const idx = row * cols + col;
+        data[col][row] = bins[idx];
+      }
+    }
+
+    console.log(data);
+
+    const config: HeatmapConfig = {
+      xAxis: { labels: xLabels },
+      yAxis: { labels: yLabels },
+      colorScale: { colors: ['red', 'blue'] },
+    };
+
+    heatmap.config = config;
+    heatmap.value = { cols: data };
+    this.replaceChildren(heatmap);
+  }
 
   private _get_bounds(slot: number, quantizer: util.Quantizer): [number, number] {
     if (quantizer instanceof util.LinearQuantizer) {
@@ -106,6 +170,47 @@ export class GuiHistogram extends HTMLElement {
     }
 
     return [0, 1];
+  }
+
+  private _get_multi_bounds(slot: number, quantizer: util.MultiQuantizer): [number, number][] {
+    const result = Array.from({ length: quantizer.dimensions.length });
+    let multiplier = 1;
+    let slotId = 0;
+    let multiSlot = slot;
+    for (let index = 0; index < quantizer.dimensions.length; index++) {
+      const qt = quantizer.dimensions[index];
+      const dimSize = this._get_quantize_size(qt);
+      multiplier = multiplier * dimSize;
+      slotId = multiSlot % dimSize;
+      multiSlot = (multiSlot - slotId) / dimSize;
+
+      const bounds = this._get_bounds(slot, qt);
+      result[index] = bounds;
+    }
+
+    return result as [number, number][];
+  }
+
+  private _get_quantize_size(quantizer: util.Quantizer): number {
+    if (quantizer instanceof util.LinearQuantizer || quantizer instanceof util.LogQuantizer) {
+      return Number(quantizer.bins);
+    } else if (quantizer instanceof util.MultiQuantizer) {
+      let slots = 1;
+      for (let index = 0; index < quantizer.dimensions.length; index++) {
+        const qt = quantizer.dimensions[index];
+        const size = this._get_quantize_size(qt);
+        if (size === -1) {
+          return 0;
+        }
+        slots *= size;
+      }
+      return slots;
+    }
+    return -1;
+  }
+
+  private _is_valid_quantizer(quantier: util.Quantizer) {
+    return quantier instanceof util.LogQuantizer || quantier instanceof util.LinearQuantizer;
   }
 }
 
