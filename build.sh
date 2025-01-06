4ec866fb613b0a1ac11a062e20b7501bb024c7de
Maxime Tricoire <maxime.tricoire@datathings.com>
2025-01-06 17:26:20 +0100

    publish lib

--- build.sh
+++ build.sh
@@ -23,4 +23,10 @@ mv \
   dist/greycat.js \
   dist/sdk/web/
 
+mkdir -p libweb/web/lib/web
+mkdir -p libweb/web/webroot/web
+cp -R dist/sdk/web/types/* libweb/web/lib/web
+cp dist/sdk/web/greycat.css libweb/webroot/web
+cp dist/sdk/web/greycat.js libweb/webroot/web
+
 rm -rf dist/types
--- publish.sh
+++ publish.sh
@@ -16,12 +16,16 @@ cd dist
 file="dist.zip"
 zip -r $file sdk
 
-ROOT_URL="https://get.greycat.io/files/sdk/web"
-curl -s -X PUT -H "Authorization: $token" -T $file                               "${ROOT_URL}/${BRANCH}/${VERSION_MAJOR_MINOR}/${VERSION}.zip"
-curl -s -X PUT -H "Authorization: $token" -T $file                               "${ROOT_URL}/${BRANCH}/latest.zip"
-curl -s -X PUT -H "Authorization: $token" -T sdk/web/package.tgz                 "${ROOT_URL}/${BRANCH}/${VERSION_MAJOR_MINOR}/${VERSION}.tgz"
-curl -s -X PUT -H "Authorization: $token" -T sdk/web/greycat.css                 "${ROOT_URL}/${BRANCH}/${VERSION_MAJOR_MINOR}/${VERSION}.css"
-curl -s -X PUT -H "Authorization: $token" -T sdk/web/greycat.js                  "${ROOT_URL}/${BRANCH}/${VERSION_MAJOR_MINOR}/${VERSION}.js"
-curl -s -X PUT -H "Authorization: $token" -T sdk/web/greycat.esm.js              "${ROOT_URL}/${BRANCH}/${VERSION_MAJOR_MINOR}/${VERSION}.esm.js"
-curl -s -X PUT -H "Authorization: $token" -T sdk/web/greycat.js                  "${ROOT_URL}/${BRANCH}/${VERSION_MAJOR_MINOR}/${VERSION}.js"
-curl -s -X PUT -H "Authorization: $token" -d "${VERSION_MAJOR_MINOR}/${VERSION}" "${ROOT_URL}/${BRANCH}/latest"
+lib="lib.zip"
+zip -r $lib libweb
+
+ROOT_URL="https://get.greycat.io/files"
+curl -s -X PUT -H "Authorization: $token" -T $file                               "${ROOT_URL}/sdk/web/${BRANCH}/${VERSION_MAJOR_MINOR}/${VERSION}.zip"
+curl -s -X PUT -H "Authorization: $token" -T $file                               "${ROOT_URL}/sdk/web/${BRANCH}/latest.zip"
+curl -s -X PUT -H "Authorization: $token" -T sdk/web/package.tgz                 "${ROOT_URL}/sdk/web/${BRANCH}/${VERSION_MAJOR_MINOR}/${VERSION}.tgz"
+curl -s -X PUT -H "Authorization: $token" -T sdk/web/greycat.css                 "${ROOT_URL}/sdk/web/${BRANCH}/${VERSION_MAJOR_MINOR}/${VERSION}.css"
+curl -s -X PUT -H "Authorization: $token" -T sdk/web/greycat.js                  "${ROOT_URL}/sdk/web/${BRANCH}/${VERSION_MAJOR_MINOR}/${VERSION}.js"
+curl -s -X PUT -H "Authorization: $token" -T sdk/web/greycat.esm.js              "${ROOT_URL}/sdk/web/${BRANCH}/${VERSION_MAJOR_MINOR}/${VERSION}.esm.js"
+curl -s -X PUT -H "Authorization: $token" -T sdk/web/greycat.js                  "${ROOT_URL}/sdk/web/${BRANCH}/${VERSION_MAJOR_MINOR}/${VERSION}.js"
+curl -s -X PUT -H "Authorization: $token" -d "${VERSION_MAJOR_MINOR}/${VERSION}" "${ROOT_URL}/sdk/web/${BRANCH}/latest"
+curl -s -X PUT -H "Authorization: $token" -T $lib                                "${ROOT_URL}/web/${BRANCH}/${VERSION_MAJOR_MINOR}/${LIB}.zip"
