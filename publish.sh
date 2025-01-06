624536ff2adb726b980068653c6ec8e31274e414
Maxime Tricoire <maxime.tricoire@datathings.com>
2025-01-06 17:30:57 +0100

    fix

--- publish.sh
+++ publish.sh
@@ -11,15 +11,13 @@ sha256_hash=$(echo -n "$GET_GC_CI_PASS" | openssl dgst -sha256 | cut -d ' ' -f2)
 base64url_token=$(echo -n "ci:$sha256_hash" | base64 -w 0)
 token=$(curl -s -d "[\"${base64url_token}\", false]" -X POST https://get.greycat.io/runtime::User::login | tr -d '"')
 
+ROOT_URL="https://get.greycat.io/files"
+
 cd dist
 
 file="dist.zip"
 zip -r $file sdk
 
-lib="lib.zip"
-zip -r $lib libweb
-
-ROOT_URL="https://get.greycat.io/files"
 curl -s -X PUT -H "Authorization: $token" -T $file                               "${ROOT_URL}/sdk/web/${BRANCH}/${VERSION_MAJOR_MINOR}/${VERSION}.zip"
 curl -s -X PUT -H "Authorization: $token" -T $file                               "${ROOT_URL}/sdk/web/${BRANCH}/latest.zip"
 curl -s -X PUT -H "Authorization: $token" -T sdk/web/package.tgz                 "${ROOT_URL}/sdk/web/${BRANCH}/${VERSION_MAJOR_MINOR}/${VERSION}.tgz"
@@ -28,4 +26,9 @@ curl -s -X PUT -H "Authorization: $token" -T sdk/web/greycat.js                 
 curl -s -X PUT -H "Authorization: $token" -T sdk/web/greycat.esm.js              "${ROOT_URL}/sdk/web/${BRANCH}/${VERSION_MAJOR_MINOR}/${VERSION}.esm.js"
 curl -s -X PUT -H "Authorization: $token" -T sdk/web/greycat.js                  "${ROOT_URL}/sdk/web/${BRANCH}/${VERSION_MAJOR_MINOR}/${VERSION}.js"
 curl -s -X PUT -H "Authorization: $token" -d "${VERSION_MAJOR_MINOR}/${VERSION}" "${ROOT_URL}/sdk/web/${BRANCH}/latest"
+
+cd ..
+lib="lib.zip"
+zip -r $lib libweb
+
 curl -s -X PUT -H "Authorization: $token" -T $lib                                "${ROOT_URL}/web/${BRANCH}/${VERSION_MAJOR_MINOR}/${LIB}.zip"
