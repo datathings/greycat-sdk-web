caabfc28a461db6a6f004c9dd24e69fefad804ea
Maxime Tricoire <maxime.tricoire@datathings.com>
2025-01-13 14:57:41 +0100

    fix jsx

--- src/jsx/jsx-runtime.ts
+++ src/jsx/jsx-runtime.ts
@@ -1,5 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/triple-slash-reference
-/// <reference path="../../jsx.d.ts" />
+import './jsx.js';
 
 export namespace JSX {
   export interface Element extends Node {}
