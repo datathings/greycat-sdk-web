f04ff349893f3d8479bed9d8d13de9cd3cea6293
Maxime Tricoire <maxime.tricoire@datathings.com>
2025-03-20 17:59:57 +0100

    fix build.sh

--- build.sh
+++ build.sh
@@ -29,7 +29,7 @@ mkdir -p libweb/lib/web
 mkdir -p libweb/webroot/web
 
 cp dist/sdk/web/greycat.d.ts       libweb/lib/web/
-cp -r dist/sdk/types               libweb/lib/web/
+cp -r dist/sdk/web/types           libweb/lib/web/
 cp vendor/*                        libweb/lib/web/
 cp dist/sdk/web/greycat.css        libweb/webroot/web/
 cp dist/sdk/web/greycat.web.esm.js libweb/webroot/web/greycat.js
