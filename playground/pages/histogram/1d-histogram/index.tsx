0875a7a978eed23b18769fc67022c64ab3e4ffe4
Max <max.cruz@datathings.com>
2025-05-12 15:57:24 +0200

    fix histo prop name

--- playground/pages/histogram/1d-histogram/index.tsx
+++ playground/pages/histogram/1d-histogram/index.tsx
@@ -6,6 +6,6 @@ await gc.sdk.init();
 const bins = await gc.project.one_d_histogram_bins();
 document.body.appendChild(
   <app-layout title="Histogram 1D">
-    <gui-histogram bins={bins}> </gui-histogram>
+    <gui-histogram value={bins}> </gui-histogram>
   </app-layout>,
 );
