607c9089eeb0e38810cf22717fb95b41b3608dae
Max <max.cruz@datathings.com>
2025-06-02 16:19:33 +0200

    fix chart selection display bug

--- src/web/components/chart/chart.ts
+++ src/web/components/chart/chart.ts
@@ -1210,8 +1210,7 @@ export class GuiChart extends GuiElement {
           }
         } else {
           if (this._config.xAxis.scale === 'time') {
-            const [from, to] = xScale.range();
-            const span = Math.abs(+xScale.invert(to) - +xScale.invert(from));
+            const span = Math.abs(to - from);
             const specifier = smartTimeFormatSpecifier(span);
             fromStr = this._config.xAxis.cursorFormat(from, specifier);
             toStr = this._config.xAxis.cursorFormat(to, specifier);
